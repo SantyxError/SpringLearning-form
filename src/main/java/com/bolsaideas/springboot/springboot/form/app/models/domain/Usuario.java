639cc11fcf5c66fcfa92b5284e01bb3bb75fa50c
Santiago <sanbemal@gmail.com>
2023-08-03 08:59:45 +0200

    67. Validación personalizada usando anotaciones

--- src/main/java/com/bolsaideas/springboot/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsaideas/springboot/springboot/form/app/models/domain/Usuario.java
@@ -1,5 +1,6 @@
 package com.bolsaideas.springboot.springboot.form.app.models.domain;
 
+import com.bolsaideas.springboot.springboot.form.app.validation.IdentificadorRegex;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -8,6 +9,7 @@ import jakarta.validation.constraints.Size;
 public class Usuario {
 
     // @Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
+    @IdentificadorRegex
     private String identificador;
 
     //@NotEmpty
---
+++ src/main/java/com/bolsaideas/springboot/springboot/form/app/validation/IdentificadorRegexValidador.java
@@ -0,0 +1,21 @@
+package com.bolsaideas.springboot.springboot.form.app.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class IdentificadorRegexValidador implements ConstraintValidator<IdentificadorRegex, String> {
+
+    @Override
+    public void initialize(IdentificadorRegex constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value.matches("[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")) {
+            return true;
+        }
+
+        return false;
+    }
+}
