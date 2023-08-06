ef950f41a42ac6ff6a2bf6243cf19ec09385f291
Santiago <sanbemal@gmail.com>
2023-08-06 20:37:59 +0200

    77. Llenando lista select con Map

--- src/main/java/com/bolsaideas/springboot/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsaideas/springboot/springboot/form/app/controllers/FormController.java
@@ -20,7 +20,9 @@ import org.springframework.web.bind.support.SessionStatus;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @SessionAttributes("usuario")
@@ -43,6 +45,19 @@ public class FormController {
 	@ModelAttribute("paises")
 	public List<String> paises() {
 		return Arrays.asList("España", "Mexico", "Chile", "Argentina", "Perú", "Colombia", "Venezuela");
+	}
+
+	@ModelAttribute("paisesMap")
+	public Map<String, String> paisesMap() {
+		Map<String, String> paises = new HashMap<String, String>();
+		paises.put("ES", "España");
+		paises.put("MX", "Mexico");
+		paises.put("CL", "Chile");
+		paises.put("AR", "Argentina");
+		paises.put("PE", "Perú");
+		paises.put("CO", "Colombia");
+		paises.put("VE", "Venezuela");
+		return paises;
 	}
 
 	@GetMapping("/form")
