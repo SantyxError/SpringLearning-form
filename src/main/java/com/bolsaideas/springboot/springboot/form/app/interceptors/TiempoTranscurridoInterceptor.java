a7a955429cd2ff9e64775f2116826102b8a18e8e
Santiago <sanbemal@gmail.com>
2023-08-14 08:30:09 +0200

    95. Implementando interceptor TiempoTranscurrido

--- src/main/java/com/bolsaideas/springboot/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsaideas/springboot/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -2,17 +2,40 @@ package com.bolsaideas.springboot.springboot.form.app.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Random;
+
 @Component
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
+
+	private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
+
 	@Override public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		logger.info("TiempoTranscurridoInterceptor: preGandle() entrando...");
+		long tiempoInicio = System.currentTimeMillis();
+		request.setAttribute("tiempoInicio", tiempoInicio);
+
+		Random random = new Random();
+		Integer demora = random.nextInt(500);
+		Thread.sleep(demora);
+
 		return true;
 	}
 
 	@Override public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		long tiempoFin = System.currentTimeMillis();
+		long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
+		long tiempoTranscurrido = tiempoFin - tiempoInicio;
 
+		if (modelAndView!=null){
+			modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
+		}
+		logger.info("TiempoTranscurrido: postGandle() saliendo..." + tiempoTranscurrido + " milisegundos");
+		logger.info("TiempoTranscurridoInterceptor: postGandle() saliendo...");
 	}
 }
